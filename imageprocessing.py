ffc3db1f57b60cb4a78aaef28f5698b3ef9c361e
Pegasi Assocation <128257630+BlackBoyZeus@users.noreply.github.com>
2023-07-14 23:23:19 -0700

    Update imageprocessing.py

--- imageprocessing.py
+++ imageprocessing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 class ImageProcessingLibrary:
     def __init__(self):
@@ -10,46 +11,35 @@ class ImageProcessingLibrary:
         return blurred_image
 
     def create_gaussian_kernel(self, kernel_size, sigma=1.0):
-        kx = np.linspace(-(kernel_size[0] // 2), kernel_size[0] // 2, kernel_size[0])
-        ky = np.linspace(-(kernel_size[1] // 2), kernel_size[1] // 2, kernel_size[1])
-        xx, yy = np.meshgrid(kx, ky)
-        kernel = np.exp(-(xx ** 2 + yy ** 2) / (2 * sigma ** 2))
+        x, y = np.meshgrid(np.linspace(-1, 1, kernel_size[0]), np.linspace(-1, 1, kernel_size[1]))
+        distance = np.sqrt(x**2 + y**2)
+        kernel = np.exp(-0.5 * (distance / sigma)**2)
         kernel /= np.sum(kernel)
         return kernel
 
     def convolve(self, image, kernel):
-        height, width = image.shape[:2]
-        kernel_height, kernel_width = kernel.shape[:2]
-        pad_height = kernel_height // 2
-        pad_width = kernel_width // 2
-        padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), mode='edge')
-        convolved_image = np.zeros_like(image)
-        for i in range(height):
-            for j in range(width):
-                patch = padded_image[i:i+kernel_height, j:j+kernel_width]
-                result = np.sum(patch * kernel)
-                convolved_image[i, j] = result
+        convolved_image = cv2.filter2D(image, -1, kernel)
         return convolved_image
 
     def canny_edge_detection(self, image, threshold1, threshold2):
-        # Implement Canny edge detection algorithm
         # Step 1: Apply Gaussian blur to reduce noise
-        blurred_image = self.gaussian_blur(image, kernel_size=(3, 3))
-        
+        blurred_image = self.gaussian_blur(image, kernel_size=(5, 5))
+
         # Step 2: Compute gradients using Sobel operator
-        gradient_x = self.convolve(blurred_image, np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]))
-        gradient_y = self.convolve(blurred_image, np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]))
-        
+        gradient_x = cv2.Sobel(blurred_image, cv2.CV_64F, 1, 0, ksize=3)
+        gradient_y = cv2.Sobel(blurred_image, cv2.CV_64F, 0, 1, ksize=3)
+
         # Step 3: Compute gradient magnitude and direction
         gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
-        gradient_direction = np.arctan2(gradient_y, gradient_x)
-        
+        gradient_direction = np.arctan2(gradient_y, gradient_x) * (180 / np.pi)
+        gradient_direction = np.where(gradient_direction < 0, gradient_direction + 180, gradient_direction)
+
         # Step 4: Apply non-maximum suppression to thin edges
         suppressed_image = self.non_max_suppression(gradient_magnitude, gradient_direction)
-        
+
         # Step 5: Apply double thresholding and edge linking
         edges = self.edge_linking(suppressed_image, threshold1, threshold2)
-        
+
         return edges
 
     def non_max_suppression(self, gradient_magnitude, gradient_direction):
@@ -61,7 +51,7 @@ class ImageProcessingLibrary:
             for j in range(1, cols - 1):
                 angle = gradient_direction[i, j]
 
-                # Compare gradient magnitude with neighboring pixels along the direction of the gradient
+                # Determine the two neighboring pixels to compare the gradient magnitude with
                 if (0 <= angle < 22.5) or (157.5 <= angle <= 180):
                     q = gradient_magnitude[i, j+1]
                     r = gradient_magnitude[i, j-1]
@@ -75,6 +65,7 @@ class ImageProcessingLibrary:
                     q = gradient_magnitude[i-1, j-1]
                     r = gradient_magnitude[i+1, j+1]
 
+                # Suppress the pixel if it is not the maximum among the neighboring pixels along the gradient direction
                 if gradient_magnitude[i, j] >= q and gradient_magnitude[i, j] >= r:
                     suppressed_image[i, j] = gradient_magnitude[i, j]
 
@@ -82,20 +73,15 @@ class ImageProcessingLibrary:
 
     def edge_linking(self, edges, low_threshold, high_threshold):
         # Implement edge linking algorithm (Hysteresis thresholding)
-        rows, cols = edges.shape
-        strong_edges = np.zeros_like(edges)
-        weak_edges = np.zeros_like(edges)
+        thresholded_image = np.zeros_like(edges)
+        thresholded_image[(edges >= low_threshold) & (edges < high_threshold)] = 128
 
-        strong_edges[edges >= high_threshold] = 255
-        weak_edges[(edges >= low_threshold) & (edges < high_threshold)] = 255
+        strong_edges = cv2.threshold(edges, high_threshold, 255, cv2.THRESH_BINARY)[1]
+        _, weak_edges = cv2.threshold(thresholded_image, 0, 255, cv2.THRESH_BINARY)
+        connected_edges = cv2.Canny(weak_edges, low_threshold, high_threshold)
 
-        for i in range(1, rows - 1):
-            for j in range(1, cols - 1):
-                if weak_edges[i, j] == 255:
-                    if np.max(strong_edges[i-1:i+2, j-1:j+2]) == 255:
-                        strong_edges[i, j] = 255
-
-        return strong_edges
+        edges = cv2.bitwise_or(strong_edges, connected_edges)
+        return edges
 
     def thresholding(self, image, threshold):
         # Implement thresholding algorithm
@@ -122,10 +108,8 @@ class ImageProcessingLibrary:
 # Usage example
 image = ...  # Load or create an image
 image_processor = ImageProcessingLibrary()
-blurred_image = image_processor.gaussian_blur(image, kernel_size=(3, 3))
-gradient_magnitude, gradient_direction = image_processor.calculate_gradients(blurred_image)
-suppressed_image = image_processor.non_max_suppression(gradient_magnitude, gradient_direction)
-edges = image_processor.edge_linking(suppressed_image, low_threshold=50, high_threshold=150)
+blurred_image = image_processor.gaussian_blur(image, kernel_size=(5, 5))
+edges = image_processor.canny_edge_detection(blurred_image, threshold1=50, threshold2=150)
 thresholded_image = image_processor.thresholding(image, threshold=128)
 contours = image_processor.find_contours(thresholded_image)
 result_image = image_processor.draw_contours(image, contours)
